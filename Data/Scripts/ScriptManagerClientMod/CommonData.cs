8409ab828342d06ffea5eaee83863e2c154d97d6
Tobias Knoeppler <3knoeppl@informatik.uni-hamburg.de>
2018-08-09 14:24:29 +0200

    Implement message handling and protocol

---
+++ Data/Scripts/ScriptManagerClientMod/CommonData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptManagerClientMod
+{
+    static class CommonData
+    {
+        public static Dictionary<long, string> Scripts = new Dictionary<long, string>
+        {
+            { -1, "NONE" }
+        };
+        public static Dictionary<long, string> ScriptBodies = new Dictionary<long, string>
+        {
+            { -1, "" }
+        };
+        public static Guid GUID { get; } = new Guid("5f35403d-2150-4d83-9ef8-3510f25bbdf3");
+
+    }
+}
---
+++ Data/Scripts/ScriptManagerClientMod/Requests/RecompileRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace ScriptManagerClientMod
+{
+    [ProtoContract]
+    class RecompileRequest : RemoteRequest
+    {
+        [ProtoMember(5)]
+        public long pbId = 0;
+
+        public RecompileRequest() { }
+
+        public RecompileRequest(ulong sender, long programmableBlock) : base(sender, RemoteRequestType.RECOMPILE)
+        {
+            pbId = programmableBlock;
+        }
+    }
+}
